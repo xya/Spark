2927bc8de12e5203389f2c09b08628aa353d9c65
Pierre-André Saulais <pasaulais@free.fr>
2010-07-25 20:32:54 +0200

    Updated the PKGBUILD for release 0.0.4

--- build/archlinux/updatepkg.sh
+++ build/archlinux/updatepkg.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-VERSION="$1"
+if [ $# = 0 ]; then
+    VERSION=$(tr -d '\n' < ../VERSION)
+else
+    VERSION="$1"
+fi
 CHECKSUMS=$(makepkg -g 2>/dev/null | grep md5sums)
 sed -i "s/pkgver=.*/pkgver=$VERSION/" PKGBUILD
 sed -i "s/md5sums=(.*)/$CHECKSUMS/" PKGBUILD
